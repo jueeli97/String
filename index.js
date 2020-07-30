b5eb0f20749b8f50a435a674cec4b04f9e306424
unknown <jueeli97@gmail.com>
2020-07-30 22:47:11 +0530

    String in JS
    Signed-off-by: unknown <jueeli97@gmail.com>

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+// string
+
+// Mutable
+let strValue1 = "john";
+let strValue2 = "wright";
+let result = strValue1+ " " + strValue2;
+console.log(result); // john wright
+
+//Immutable
+// STRING INTERPOLATION
+let result1 = `${strValue1} ${strValue2}
+`;
+console.log(result1); // john wright
+
+
+
+////////////////////////////////
+
+let numValue1 = 2;
+let numValue2 = 4;
+let numValue3 = 5;
+let numValue4 = 8;
+
+ strValue1 = "6";
+
+ result = numValue1+ numValue2 + strValue1 + numValue3 + numValue4;
+ console.log(result); // 6658
+
+ result =  strValue1 * numValue4;
+ console.log(result); // 48
+
+ 
